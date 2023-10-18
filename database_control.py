04c06838773c9a63138628e1d8b1fe015f349ac2
Shawn Jones <sbradenjones16@gmail.com>
2023-10-18 16:25:25 -0700

    working on api for adding and saving data

--- database_control.py
+++ database_control.py
@@ -76,7 +76,7 @@ class DataBaseHandler(threadWrapper):
         table_feilds = self.__tables[args[0]].get_fields()
         db_command = f"CREATE TABLE IF NOT EXISTS {table_name} ("
         #this line is add as a way to index the data base
-        db_command += "[time_stamp] BIGINT PRIMARY KEY" 
+        db_command += "[table_idx] INT PRIMARY KEY" 
 
         #iter for every feild in this data group. The feilds come from the dataTypes.dtobj file. 
         for feild_name in table_feilds:
@@ -100,7 +100,7 @@ class DataBaseHandler(threadWrapper):
             self.__coms.print_message(str(error) + " Command send to db: " + db_command, 0) 
             self.__logger.send_log("Failed to created table: " + db_command + str(error))
             self.__coms.print_message("Failed to created table: " + db_command + str(error), 0)
-    def insert_data(self, args):
+    def insert_data(self, args, idx_in = -1):
         '''
             This func takes in the table_name to insert and a list of data, 
             the list must be in the same order that is defined in the .dtobj file.
@@ -108,9 +108,20 @@ class DataBaseHandler(threadWrapper):
             the second is the data
             Args:
                 [0] : table name
-                [1] : data
+                [1] : list of data
         '''
-        db_command = f"INSERT INTO {args[0]} (time_stamp"
+        #first step get the last saved idx
+        if idx_in == -1:
+            self.__c.execute(f"SELECT * FROM {args[0]} WHERE table_idx = (SELECT max(table_idx) FROM {args[0]})")
+            data = pd.DataFrame(self.__c.fetchall()) 
+            if data.empty:
+                idx = 0
+            else :
+                idx  = data[0][0] + 1
+        else :
+            idx = idx_in # if we pass the index in we save time
+        # idx = 0 #if the above command fails it means there is no data saved into the data base and we need to start the index at 0.
+        db_command = f"INSERT INTO {args[0]} (table_idx"
         #get the data type obj and then get the feilds list
         feilds = self.get_feilds([self.get_data_type([args[0]])]) 
         for feild_name in feilds:
@@ -118,23 +129,22 @@ class DataBaseHandler(threadWrapper):
             if feild_name!= "igrnoed feild" and feilds[feild_name][1] != "NONE" : 
                 db_command += ", "
                 db_command += f"{feild_name}"
-            db_command +=  ") "
-            db_command += f"VALUES ({time.time()}"
+        db_command +=  ") "
+        db_command += f"VALUES ({idx}"
+
         for val in args[1]: #args[1] is the data
             db_command += ", "
             # for sql str has a special format so we need the if statment
-            if isinstance(val) != str:
+
+            if not isinstance(val, str):
                 db_command += f"{val}"
             else :
                 db_command += f"'{val}'" #Notice that there is '' on this line.           
-        db_command += ")"
+        db_command += ");"
         try:
             self.__c.execute(db_command)
-            self.__conn.commit()
-            self.__logger.send_log("INSERT COMMAND: " + db_command)
-            self.__coms.print_message("INSERT COMMAND: " + db_command, 2)
         except Exception as error:
-            self.__coms(str(error) + " Command send to db: " + db_command, 0) 
+            self.__coms.print_message(str(error) + " Command send to db: " + db_command, 0) 
             self.__logger.send_log(str(error) + " Command send to db: " + db_command)
             # pylint: disable=w0707
             # pylint: disable=w0719
@@ -180,12 +190,16 @@ class DataBaseHandler(threadWrapper):
             and the second is the start time for collecting the data
             ARGS:
                 [0] table name
-                [1] time stamp
+                [1] table_idx (starting indx)
+            RETURNS:
+                html string with data
+            NOTE: This function is NOT meant for larg amouts of data! Make request small request to this function of it will take a long time. 
         '''
+        print("Get request")
         message = f"<h1>{args[0]}: " 
         try :
             #from and run db command
-            db_command = f"SELECT * FROM {args[0]} WHERE time_stamp >= {str(args[1])} ORDER BY time_stamp"
+            db_command = f"SELECT * FROM {args[0]} WHERE table_idx >= {str(args[1])} ORDER BY table_idx"
             self.__c.execute(db_command)
             self.__logger.send_log("Query command recived: "  + db_command)
             self.__coms.print_message("Query command recived: "  + db_command, 2)
@@ -194,11 +208,11 @@ class DataBaseHandler(threadWrapper):
             self.__logger.send_log(str(error) + " Command send to db: " + db_command)
             return "<p> Error getting data </p>"
         #get cols 
-        cols = ["Time Stored "] # add time stamp to the cols lis
+        cols = ["Table Index "] # add table_idx to the cols lis
         cols += self.get_feilds_list([self.get_data_type([args[0]])])
         message += f"{cols}</h1> "
         #fetch and convert the data into a pandas data frame.
-        data = pd.DataFrame(self.__c.fetchall(), columns=cols)        
+        data = pd.DataFrame(self.__c.fetchall(), columns=cols)      
         for idx in range(len(data)):
             message += "<p>"
             for i in range(len(cols) - 1): #itrate for all but last col
@@ -223,8 +237,9 @@ class DataBaseHandler(threadWrapper):
         if 'list ' not in out_feild_type:
             new_data_type.add_feild(name, 0, out_feild_type)
         else :
+            new_data_type.add_feild('index_internal', 0, 'int')
             new_data_type.add_feild(name, 0, out_feild_type.replace('list ', ''))
-            new_data_type.add_feild('index', 0, 'int')
+            new_data_type.add_feild('Data_group', 0, 'string')
 
         self.__tables[table_name] = new_data_type
 
@@ -246,16 +261,77 @@ class DataBaseHandler(threadWrapper):
         table_feilds = self.__tables[args[0]].get_fields()
         #Open the archived file
         self.__dataFile = open("database/dataTypes.dtobj", 'a') # pylint: disable=r1732
-        self.__dataFile.write("\n" + table_name + "\n")
+        self.__dataFile.write(table_name + "\n")
         for feild in table_feilds:
             feild_info = self.__tables[table_name].get_field_info(feild)
             self.__dataFile.write(f"    {feild}:{feild_info[0]} > {feild_info[1]}\n")
         self.__dataFile.close()
         #Open the archived back up file
         self.__dataFile = open("database/dataTypes_backup.dtobj", 'a') # pylint: disable=r1732
-        self.__dataFile.write("\n" + table_name + "\n")
+        self.__dataFile.write(table_name + "\n")
         for feild in table_feilds:
             feild_info = self.__tables[table_name].get_field_info(feild)
             self.__dataFile.write(f"    {feild}:{feild_info[0]} > {feild_info[1]}\n")
         self.__dataFile.close()
-                                                                                    
+    def add_save_data_group(self, args):
+        '''
+            This function takes in a list of data to store as a group
+
+            ARGS:
+                args[0] : table name
+                args[1] : group (ussally the input to produce the data)
+                args[2] : list of data to store
+            NOTE: If there is no group the logic will infur that it is NOT a list type.
+        '''
+
+        #get the index
+        self.__c.execute(f"SELECT * FROM {args[0]} WHERE table_idx = (SELECT max(table_idx) FROM {args[0]})")
+        row = pd.DataFrame(self.__c.fetchall()) 
+        if row.empty:
+            idx = 0
+        else :
+            idx  = row[0][0] + 1
+
+        if not 'NO GROUP' in args[1]: #this guy handles when a single input produces many outputs.
+            sub_idx = 0
+            for data in args[2]:
+                self.insert_data([args[0], [data, sub_idx, args[1]]], idx_in= idx)
+                idx += 1
+                sub_idx += 1
+            self.__conn.commit()
+            self.__coms.print_message(f"Inserted Data groug {args[1]}.")
+        else :
+            for val in args[2]:
+                self.insert_data([args[0], [val]], idx_in = idx) # this is the command to save a single list of values.
+                idx += 1
+            self.__conn.commit()
+            self.__coms.print_message(f"Inserted Data.")
+    def get_data_large(self, args):
+        '''
+            args is a list where the fist index is the table name 
+            and the second is the start time for collecting the data
+            ARGS:
+                [0] table name
+                [1] table_idx (starting indx)
+            RETURNS:
+                pandas data obj
+            NOTE: This function IS for larg amouts of data! 
+        '''
+        message = f"<h1>{args[0]}: " 
+        try :
+            #from and run db command
+            db_command = f"SELECT * FROM {args[0]} WHERE table_idx >= {str(args[1])} ORDER BY table_idx"
+            self.__c.execute(db_command)
+            self.__logger.send_log("Query command recived: "  + db_command)
+            self.__coms.print_message("Query command recived: "  + db_command, 2)
+        except Exception as error: # pylint: disable=w0718
+            self.__coms.print_message(str(error) + " Command send to db: " + db_command, 0)
+            self.__logger.send_log(str(error) + " Command send to db: " + db_command)
+            return "<p> Error getting data </p>"
+        #get cols 
+        cols = ["Table Index "] # add table_idx to the cols lis
+        cols += self.get_feilds_list([self.get_data_type([args[0]])])
+        message += f"{cols}</h1> "
+        #fetch and convert the data into a pandas data frame.
+        data = pd.DataFrame(self.__c.fetchall(), columns=cols)  
+        return data
